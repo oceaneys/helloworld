7fc7fd5c5c18ef5169ba7fe3410c25e2fd53c861
heavy <heavy@192.168.5.15>
2024-09-26 19:18:36 +0800

    introduce argument

--- main.py
+++ main.py
@@ -1,10 +1,27 @@
 #!/usr/bin/env
 # -*- coding:utf-8 -*-
 
+import argparse
+from time import sleep
+
+def arg_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--type", type=str, default="single")
+    return parser.parse_args()
+
+
 if __name__ == "__main__":
-	try:
-		print("Hello World")
-	except KeyboardInterrupt:
-		print("KeyboardInterrupt")
-	except Exception as e:
-		print(e)
+    try:
+        args = arg_parser()
+        if args.type == "single":
+             print("Hello World")
+        elif args.type == "loop":
+            while True:
+                sleep(1)
+                print("Hello World")
+        else:
+            print("Invalid type")
+    except KeyboardInterrupt:
+        print("KeyboardInterrupt")
+    except Exception as e:
+        print(e)
