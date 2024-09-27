6214e2fb864aa595d51e1adf68585748ca817a94
hhhyyy <343976857@qq.comm>
2024-09-27 22:35:24 +0800

    introduce logging

--- main.py
+++ main.py
@@ -3,19 +3,28 @@
 
 import argparse
 from time import sleep
+import logging
+
+logger = logging.getLogger(__name__)
 
 def arg_parser():
     parser = argparse.ArgumentParser()
     parser.add_argument("--type", type=str, default="single")
     return parser.parse_args()
 
+def setup_logging():
+    logging.basicConfig(filename='output.log', format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+    logger.setLevel(logging.INFO)
 
 if __name__ == "__main__":
     try:
         args = arg_parser()
+        setup_logging()
         if args.type == "single":
              print("Hello World")
+             logger.info("output type: single")
         elif args.type == "loop":
+            logger.info("output type: loop")
             while True:
                 sleep(1)
                 print("Hello World")
@@ -23,5 +32,6 @@ if __name__ == "__main__":
             print("Invalid type")
     except KeyboardInterrupt:
         print("KeyboardInterrupt")
+        logger.info("KeyboardInterrupt")
     except Exception as e:
         print(e)
